2fe8351f61deac58f73bbfc44cf4b2a4f0d784fa
Victor Arruda <thesalxtv@gmail.com>
2025-08-29 09:16:23 -0300

    Resolução do Desafio .NET POO Smartphones

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+                                
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando Aplicativo {nomeApp} no iPhone {Numero}! ");
+            InformacaoTelefone();
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i1 = new Iphone("9999-9999", "Iphone Lá Ele Pró Máx", "QLQRIMEIIPHONE", 16);
+{
+    i1.Ligar();
+    i1.ReceberLigacao();
+    i1.InstalarAplicativo("Whatsapp");
+}
+
+Nokia n1 = new Nokia("8888-8888", "Nokia Azul Indestrutível", "QLQRIMEINOKIA", 128);
+{
+    n1.Ligar();
+    n1.ReceberLigacao();
+    n1.InstalarAplicativo("Windows Mobile");
+}
